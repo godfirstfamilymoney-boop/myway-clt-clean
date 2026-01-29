bd8c1abe14e238ed030616b28d70786ffd61d1d6
godfirstfamilymoney-boop <godfirstfamilymoney@gmail.com>
2026-01-28 22:19:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,26 +12,10 @@ const db = firebase.firestore();
 window.onload = function () {
 
   // Button Click
-  document.getElementById("requestRide").onclick = async () => {
-
-    const pickup = document.getElementById("pickup").value;
-    const dropoff = document.getElementById("dropoff").value;
-
-    if (!pickup || !dropoff) {
-      alert("Enter pickup + dropoff!");
-      return;
-    }
-
-    // ✅ Save Ride in Firestore
-    await db.collection("rides").add({
-      pickup: pickup,
-      dropoff: dropoff,
-      status: "Pending",
-      createdAt: new Date(),
-    });
-
-    // ✅ Redirect to Status Page
-    window.location.href = "status.html";
-  };
+ document.getElementById("requestRide").onclick = function () {
+  alert("Button works!");
+  window.location.href = "status.html";
+};
 
 };
+
