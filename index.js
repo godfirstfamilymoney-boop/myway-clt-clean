b1b75b05066cea6c0e8616dd0181a8bb6123aed8
godfirstfamilymoney-boop <godfirstfamilymoney@gmail.com>
2026-01-29 17:17:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyCbSpg1Xh5Cg9fGNgO-tsw__O8Y7VDT_HM",
   authDomain: "myway-clt-final.firebaseapp.com",
   projectId: "myway-clt-final",
+  storageBucket: "myway-clt-final.firebasestorage.app",
+  messagingSenderId: "207622732846",
+  appId: "1:207622732846:web:92935a0cef94b261b773bf"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -20,6 +23,7 @@ window.onload = function () {
 };
 
 };
+
 
 
 
