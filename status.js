d62339808645ffa73139d2874c76fb48b07516db
godfirstfamilymoney-boop <godfirstfamilymoney@gmail.com>
2026-02-07 21:59:41 -0500

    Update status.js

--- status.js
+++ status.js
@@ -1,20 +1,13 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCbSpg1Xh5Cg9fGNgO-tsw__O8Y7VDT_HM",
-  authDomain: "myway-clt-final.firebaseapp.com",
-  projectId: "myway-clt-final",
-};
+alert("status.js running");
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
 const db = firebase.firestore();
-
 const rideId = localStorage.getItem("rideId");
 
 if (!rideId) {
   document.getElementById("statusText").innerText = "No ride found";
 }
 
+// LIVE updates
 db.collection("rides").doc(rideId)
 .onSnapshot((doc) => {
 
@@ -26,6 +19,7 @@ db.collection("rides").doc(rideId)
   document.getElementById("dropoff").innerText = ride.dropoff;
   document.getElementById("statusText").innerText = ride.status;
 
+  // Lights
   document.getElementById("pendingDot").style.opacity =
     ride.status === "Pending" ? "1" : "0.3";
 
@@ -35,14 +29,10 @@ db.collection("rides").doc(rideId)
   document.getElementById("completeDot").style.opacity =
     ride.status === "Completed" ? "1" : "0.3";
 
+  // Go to receipt when done
   if (ride.status === "Completed") {
     setTimeout(() => {
       window.location.href = "receipt.html";
     }, 1500);
   }
 });
- if (ride.status === "Completed") {
-    setTimeout(() => {
-      window.location.href = "receipt.html";
-    }, 1500);
-  }
